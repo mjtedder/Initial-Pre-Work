59925fffcf801e50c123513492316564c0e08d83
mjtedder <mjtedder@gmail.com>
2019-03-26 20:35:40 -0600

    Store values in JS

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,16 @@
+/*
+
+The assignment operator allows us to store one of the 7 data types as a value in a JavaScript variable.  Assignment
+goes from right to left.  So everything to the right of the assignment operator (=) is assigned to the variable to the left
+of the assignment operator (=).  We can also reassign variable values once they're declared using the assignment
+operator.  On line 16 we reassigned the variable b to be equal to the value of the variable a (7).
+
+*/
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a;
