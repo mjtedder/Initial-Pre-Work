5fe157ab610586df94b29988b24e2881f6b5dfb9
mjtedder <mjtedder@gmail.com>
2019-03-27 13:52:30 -0600

    Use single quotes for string

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,10 @@
+/*
+
+JavaScript reads single quotes and double quotes the same, but it is useful to incorporate single quotes when
+we have a quote within a string such as var quote = 'John said, "I love coding!".'
+
+With this method, we don't need to use \ to escape literal quotes.
+
+*/
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
