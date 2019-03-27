456416562b78bb2947d07d02e6cc2a64b00db576
mjtedder <mjtedder@gmail.com>
2019-03-27 16:27:50 -0600

    Create multi-dimensional array

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,10 @@
+/*
+
+In this lesson, we created a variable called myList that contained a multi-dimensional array with 5
+sub-arrays, each with a string of the item for a shopping list, and then an integer representing the quantity of that
+item we need.
+
+*/
+
+
+var myList = [["Ramen", 12],["Cola", 4],["Gelato", 7],["Eggs", 14],["Salmon", 2]];
