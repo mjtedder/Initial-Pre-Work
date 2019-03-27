702ebaa6a4d6b4d549e630dd74ba23d6299e01ba
mjtedder <mjtedder@gmail.com>
2019-03-27 15:02:51 -0600

    Use bracket notation

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,16 @@
+/*
+
+We can use bracket notation to access the data inside arrays.  It also uses zero-based indexing, which means
+the first piece of data in the array is accessed by using myArray[0]
+
+*/
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
