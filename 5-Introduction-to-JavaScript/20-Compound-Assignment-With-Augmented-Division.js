6f32b6be47a7532689b3bf21975b498df573678d
mjtedder <mjtedder@gmail.com>
2019-03-27 13:33:00 -0600

    Use augmented division

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,17 @@
+/*
+
+We can also use the augmented division operator to assign and perform division on a variable.
+
+For example, a = 4 because 48 / 12 = 4.
+
+*/
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
