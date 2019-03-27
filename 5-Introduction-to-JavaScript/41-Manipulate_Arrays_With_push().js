8bfd37102ce2ae64bf4ee08584a924fc60ecaa5b
mjtedder <mjtedder@gmail.com>
2019-03-27 16:02:41 -0600

    Add data to an array

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,17 @@
+/*
+
+The .push method is an array method which adds data to the end of an array.  We can add one piece of data or
+multiple pieces of data, we just add a comma to separate data.
+
+*/
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]); 
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3])
