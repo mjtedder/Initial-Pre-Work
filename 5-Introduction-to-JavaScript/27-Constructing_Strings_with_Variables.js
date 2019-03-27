061f239f469682f454d6488bb0efec4a86f07fb0
mjtedder <mjtedder@gmail.com>
2019-03-27 14:10:32 -0600

    Construct a string with variables

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,14 @@
+/*
+
+We can construct strings by saving it to a variable, and then concatenating that variable onto a string.
+This is useful when we want to insert more than one variable into a string.
+
+*/
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Matt";
+var myStr = "My name is " + myName + " and I am well!";
