5eb60182acdc45b2c10cd724f0de6bbc84ae7fee
mjtedder <mjtedder@gmail.com>
2019-03-27 13:23:21 -0600

    Use augmented subtraction

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,16 @@
+/*
+
+We can use augmented subtraction (-=) to subtract a number to a variable.  This works like += operator, but
+instead subtracts a number.
+
+*/
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
