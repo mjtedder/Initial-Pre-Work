dae4a660b1a54aa9cfd5a3292e87d412cfc7543c
mjtedder <mjtedder@gmail.com>
2019-03-27 12:52:21 -0600

    Find remainder of 11

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,18 @@
+/*
+The remainder (%) operator is used to give the remainder of the division between two numbers.
+
+In the example below:
+
+11 % 3 = 2 because
+Math.floor(11 / 3) = 3 (quotient)
+3 * 3 = 9
+11 - 9 = 2 (Remainder)
+
+We can also use the remainder (%) operator to check whether a number is even or odd by checking the
+remainder of the division of the number by 2.
+
+*/
+
+// Only change code below this line
+
+var remainder = 11 % 3;
