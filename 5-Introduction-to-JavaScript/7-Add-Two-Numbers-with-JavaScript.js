0583e7bf580b93aeb485d9c0b74289ebb2560ab4
mjtedder <mjtedder@gmail.com>
2019-03-26 21:22:03 -0600

    Set variable to add two numbers

---
+++ 5-Introduction-to-JavaScript/7-Add-Two-Numbers-with-JavaScript.js
@@ -0,0 +1,5 @@
+/* When we assign a numerical data type to a variable, we can also use a mathematical operator to add, subtract,
+multiply, or divide two numbers.  For example, we initialized a variable called sum and are adding two numbers together.
+If we were to print the variable sum, the value would read 20. */
+
+var sum = 10 + 10;
