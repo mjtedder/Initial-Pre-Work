ed389ec6d608751b372f9dc06703a5399b5d8ee7
mjtedder <mjtedder@gmail.com>
2019-03-27 14:31:27 -0600

    Reassign string value

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,17 @@
+/*
+
+In JavaScript, string values are immutable, meaning they cannot be changed once created.  Even if we want to
+alter one character in a string, we have to completed reassign it as such:
+
+var string = "Hoodbye World";
+
+string = "Goodbye World";
+
+*/
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
