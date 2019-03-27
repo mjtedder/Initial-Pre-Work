a10ef62ccb6ac624aeb1066e3da77a3bc55d847e
mjtedder <mjtedder@gmail.com>
2019-03-26 20:07:46 -0600

    Declare a variable

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,12 @@
+/* JavaScript seven different data types which can be stored in a variable.  To create a variable, we use the keyword "var"
+then the name of the variable.  We also end the statement with a semicolon, but when ES6 was introduced this is no longer necessary
+for the code to run.  Since we are practicing basic JavaScript in this module, we will end all statements with a semicolon.
+
+*/
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
+
