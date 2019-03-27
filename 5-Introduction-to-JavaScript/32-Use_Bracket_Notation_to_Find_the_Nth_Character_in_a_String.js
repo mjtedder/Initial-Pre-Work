ce99111dee8d2c2779b36b7e4458c67815491d71
mjtedder <mjtedder@gmail.com>
2019-03-27 14:34:21 -0600

    Use bracket notation

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,16 @@
+/*
+
+We can also use bracket notation to find other characters in a string.  Just remember that 0 is used to start counting
+characters, so if we want to find the first character in a string we would use, myStr[0] to access the first character.
+
+*/
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
