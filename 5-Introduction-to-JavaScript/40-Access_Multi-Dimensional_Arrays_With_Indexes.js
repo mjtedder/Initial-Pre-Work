cf791e30d73c094b8a825f51cf0c78c98e69a463
mjtedder <mjtedder@gmail.com>
2019-03-27 15:09:06 -0600

    Access multi-dimensional arrays

---
+++ 5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,13 @@
+/*
+
+We can use bracket notation to access multi-dimensional arrays.  To do this, we use multiple brackets.
+The first set of brackets refers to the first level of the array, and the following brackets refers to the next
+level of entries inside the array.
+
+*/
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
