6a6e411a30bdc7e6692430e47a9076e33c5dbdaa
mjtedder <mjtedder@gmail.com>
2019-03-27 14:02:39 -0600

    Concatenate strings

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,16 @@
+/*
+
+When we use the + operator between two strings, it will concatenate, or combine them.  For example:
+
+var fullName = "Matthew" + " Tedder";
+
+This will print as Matthew Tedder.  Be sure to include spaces when concatenating srings.
+
+*/
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
