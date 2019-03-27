1a2d123824b35b29044cc67f5621d281293b083a
mjtedder <mjtedder@gmail.com>
2019-03-27 12:23:21 -0600

    Decrease variable by 1

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,11 @@
+/*
+
+We can decrease a variable's numeric value by one using the same method, but with -- instead of ++.
+Again, this is the same as myVar = myVar - 1;
+
+*/
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
