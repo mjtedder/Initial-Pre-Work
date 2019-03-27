e4c106fedfcbdc080507909117e42e3de963365f
mjtedder <mjtedder@gmail.com>
2019-03-27 13:58:31 -0600

    Escape characters in string

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,13 @@
+/*
+
+We can escape other characters within strings in JavaScript.  This has personally
+proved useful for me when I work in the terminal in node.JS.  For example, the
+variable myStr would print like this:
+
+FirstLine
+    \SecondLine
+ThirdLine   
+
+*/
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine\t"
