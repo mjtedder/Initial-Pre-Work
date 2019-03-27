dbc7734bf92ba247a15d8298404ca476b6791eb2
mjtedder <mjtedder@gmail.com>
2019-03-27 13:21:22 -0600

    Use augmented addition

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,21 @@
+/*
+
+We can use the augmented addition (+=) operator to both assign a value to a variable and perform addition
+on that variable.  In the example below:
+
+a will return 15 because on line 19 we are telling it to be assigned the value of a + 12.  a is initialized with
+a value of 3 on line 13, so 3 + 12 = 15. So  a = 15.
+
+*/
+
+
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
