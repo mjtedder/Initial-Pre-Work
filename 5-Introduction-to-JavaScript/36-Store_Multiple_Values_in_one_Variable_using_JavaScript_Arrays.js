b0e8a971988076f41e5524232b78305ff56d4ff7
mjtedder <mjtedder@gmail.com>
2019-03-27 14:56:16 -0600

    Create array variable

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,12 @@
+/*
+
+We can use an array in JavaScript to store multiple data types in one place.  To do this, start an array declaration
+with an opening bracket ([), insert the data separated by commas(,) then close the array with a closing bracket(]).
+
+*/
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Matt", 32];
