67ba040bf935ae08b8ce4237a8d5de65da1cc73b
mjtedder <mjtedder@gmail.com>
2019-03-27 17:02:35 -0600

    Use return to get value

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,18 @@
+/*
+
+We can pass values into a function using arguments.  To get that value back, we use the keyword return to send a value
+back out of a function.
+
+*/
+
+// Example
+function minusSeven(num) {
+    return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+    return num * 5;
+}
+
+console.log(timesFive(10));
