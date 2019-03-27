c37d5afa131ffbe563d1853811cdb631582757f8
mjtedder <mjtedder@gmail.com>
2019-03-27 14:59:10 -0600

    Use array-ception

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,11 @@
+/*
+
+We can nest arrays within other arrays as well.  This is called a multi-dimensional array.
+
+*/
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Matt", 32], ["Kristen", 28]];
