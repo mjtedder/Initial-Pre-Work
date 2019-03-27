e257ebe456c29e65fa5c40b4f9fb591a86e2bf04
mjtedder <mjtedder@gmail.com>
2019-03-26 20:38:45 -0600

    Initialize variable

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+/*
+We can also initialize and assign a value to a variable on the same line using the assignment operator.
+*/
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
