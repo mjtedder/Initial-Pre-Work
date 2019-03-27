3b394dd01e5b249a08b5ee11264e386a52ba46b0
mjtedder <mjtedder@gmail.com>
2019-03-26 20:49:56 -0600

    Change variables to camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,17 @@
+/*
+In JavaScript, it is best practice to use what is called camelCase when declaring variables.  This involves having the
+first word of the variable lower-case and the subsequent words upper-case.
+
+var camelCase = "This is camelCase".
+
+*/
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
