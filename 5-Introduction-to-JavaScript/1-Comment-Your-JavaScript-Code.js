71de8771dd26fabd37c11112c4e643ac1488d730
mjtedder <mjtedder@gmail.com>
2019-03-26 19:22:14 -0600

    Add comments

--- 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
+++ 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
@@ -1,3 +1,25 @@
 // Please remember to include a summary statement with EACH FILE regarding what you learned from the lesson in this format!  I recommend keeping it at the top of each file for faster processing of your application.
 
 // Copy your code from the central panel of the freeCodeCamp page and paste it below after successfully passing the test.  You will have to click on the X at the top right of the modal that pops up to see the code. If you forget, just rerun the test.  You are also welcome to download your solution.
+
+
+/* This lesson explained the importance of comments in code.  Comments are used to explain what the code is doing.  This is good
+when you want to come back to a project and add updates, the code will remind you of what the functions do and the functionality
+you have created so far.
+
+Comments are also great when you are working on a team and a future developer is assigned to a project you worked on previously.
+It tells them what has been done so far.
+*/
+
+
+// Hello World!
+
+/*
+
+function printMessage() {
+    console.log("Hello World!");
+}
+
+printMessage();
+
+*/
