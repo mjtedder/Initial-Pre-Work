7c2b6016f27cf47d12b1832d5010c13a203eb624
mjtedder <mjtedder@gmail.com>
2019-03-27 13:42:42 -0600

    Escape literal quotes in string

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,9 @@
+/*
+
+Sometimes we want to have a literal quote inside of a string.  To prevent JavaScript from reading the double quotes
+as the end of the string we a declaring, we use the \ symbol.  This tells JavaScript to ignore the quotation marks
+and print the message to the console or to the browser.
+
+*/
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
