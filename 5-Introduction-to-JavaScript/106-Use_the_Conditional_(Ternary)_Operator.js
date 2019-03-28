f646946082ecbc963ee34881c9e367057645ad88
mjtedder <mjtedder@gmail.com>
2019-03-28 15:41:48 -0600

    Use ternary operator

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,10 @@
+/*
+The conditional or ternary operator in JavaScript is a great way to shorten if/else statements.  It can
+be written on a single line (see below).
+*/
+
+function checkEqual(a, b) {
+    return a === b ? true : false;
+}
+
+checkEqual(1, 2);
