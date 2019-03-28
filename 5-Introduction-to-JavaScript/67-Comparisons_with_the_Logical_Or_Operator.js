5673e04c5025631045561fdf39a05519b9d723a2
mjtedder <mjtedder@gmail.com>
2019-03-27 22:13:28 -0600

    Use logical or operator

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,12 @@
+// The logical or operator (||) returns true if either of the operands are true.  If not, it returns false.
+
+function testLogicalOr(val) {
+    // Only change code below this line
+    if (val < 10 || val > 20) {
+        return "Outside";
+    }
+    // Only change code above this line
+    return "Inside";
+}
+// Change this value to test
+testLogicalOr(15);
