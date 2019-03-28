bed8040e561eb99f1a34e7c046dd3ca0c8a7ac8f
mjtedder <mjtedder@gmail.com>
2019-03-28 07:50:00 -0600

    Replace if/else with return

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,10 @@
+// Since comparison operators return a boolean value of true or false, instead of using an if/else statement
+// to evaluate these values, we can just write return (a === b); to achieve the same result in less code.
+
+function isLess(a, b) {
+    // Fix this code
+    return (a < b);
+}
+
+// Change these values to test
+isLess(10, 15);
