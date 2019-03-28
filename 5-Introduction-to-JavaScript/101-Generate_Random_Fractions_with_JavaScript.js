d8ec3b236e077048db1c56660cda0c296c22240f
mjtedder <mjtedder@gmail.com>
2019-03-28 15:15:52 -0600

    Use Math.random() to generate fraction

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,8 @@
+// We can use the Math.random() function in JavaScript to generate a random number between 0 and not quite 1.  There
+// a way to generate whole numbers we will explore in future lessons.
+
+function randomFraction() {
+    // Only change code below this line.
+    return Math.random();
+    // Only change code above this line.
+}
