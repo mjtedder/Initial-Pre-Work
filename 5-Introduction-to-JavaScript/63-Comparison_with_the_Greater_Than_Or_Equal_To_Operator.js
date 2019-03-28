4e45e4cfba6ef54ac5cee92e7c644ac43e3d439e
mjtedder <mjtedder@gmail.com>
2019-03-27 21:52:15 -0600

    Use greater than or equal to operator

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,17 @@
+// Another comparison operator in JavaScript is the greater than or equal to operator (>=).  This compares whether the number
+// on the left is greater than or equal to the number on the right.
+
+function testGreaterOrEqual(val) {
+    if (val >= 20) { // Change this line
+        return "20 or Over";
+    }
+
+    if (val >= 10) { // Change this line
+        return "10 or Over";
+    }
+
+    return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
