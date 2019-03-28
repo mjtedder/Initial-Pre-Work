8dcf358b58ed986cd14e9c5d2098ad219d28fdc4
mjtedder <mjtedder@gmail.com>
2019-03-28 08:31:41 -0600

    Create dog object

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,23 @@
+/*
+
+Here we built a simple JavaScript object.  It's similar to an array, but the data is accessed and modified through
+properties.  Objects are useful for storing data that exists in the real world.  We can store properties in an object
+as strings or numbers, and these property values can be any JavaScript data type.
+
+*/
+
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+// Only change code below this line.
+var myDog = {
+    "name": "Madonna",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Salem", "Luna"]
+};
