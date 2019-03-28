d61d179652e0b0ab6d3977f154d276360ba2140e
mjtedder <mjtedder@gmail.com>
2019-03-27 21:21:31 -0600

    Create boolean function

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,15 @@
+/*
+
+Another data type in JavaScript is called booleans.  Booleans only have two values, true or false.  They are not written
+with quotes.  If you were to declare a variable with "true" or "false", the data type would be a string, not a boolean.
+
+*/
+
+function welcomeToBooleans() {
+
+    // Only change code below this line.
+
+    return true; // Change this line
+
+    // Only change code above this line.
+}
