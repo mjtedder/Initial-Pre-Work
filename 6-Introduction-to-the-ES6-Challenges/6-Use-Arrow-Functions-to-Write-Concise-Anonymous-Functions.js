53f446014b8a0853a7ea33f9d2ae4fcf5031c2e9
mjtedder <mjtedder@gmail.com>
2019-03-28 16:31:04 -0600

    Write arrow function

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,9 @@
+/*
+Arrow functions are a new feature in ES6 which are used when writing anonymous functions, or functions that
+are not used anywhere else.
+*/
+
+const magic = () => {
+    "use strict";
+    return new Date();
+};
