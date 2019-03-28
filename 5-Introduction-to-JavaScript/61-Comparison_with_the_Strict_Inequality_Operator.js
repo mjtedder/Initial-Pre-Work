04790533db86f04701529108749dcabd9033d806
mjtedder <mjtedder@gmail.com>
2019-03-27 21:42:39 -0600

    Use strict inequality operator

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,21 @@
+/*
+
+Strict inequality (!==) is the logical opposite of strict eqaulity (===).  It does not convert different data types.
+
+*/
+
+// Setup
+function testStrictNotEqual(val) {
+    // Only Change Code Below this Line
+
+    if (val !== 17) {
+
+        // Only Change Code Above this Line
+
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
