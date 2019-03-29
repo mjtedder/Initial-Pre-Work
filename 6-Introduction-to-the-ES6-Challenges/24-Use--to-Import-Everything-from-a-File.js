049b6fbce7a7927793c09fd99ab4b4802598b843
mjtedder <mjtedder@gmail.com>
2019-03-28 23:56:26 -0600

    Import all contents

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use--to-Import-Everything-from-a-File.js
@@ -0,0 +1,7 @@
+/*
+We can import all of the contents from a file using the import * syntax.  In order to do this, we have to create an
+object to receive the imported values, and specify the file name of where to import.
+*/
+
+import * as myCapitalizeModule from "capitalize_strings";
+"use strict";
