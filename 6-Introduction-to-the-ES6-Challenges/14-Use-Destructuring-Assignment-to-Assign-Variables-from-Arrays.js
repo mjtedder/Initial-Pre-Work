e49dfd1b1c03066d6ed5e3b0388b21084bffec3b
mjtedder <mjtedder@gmail.com>
2019-03-28 18:50:32 -0600

    Use destructuring assignment on array

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,16 @@
+/*
+We can also use the destructuring assignment to assign variables from array.  The difference from this between
+the spread operator is that it unpacks ALL of the contents of the array into a comma-separated list, so you can't pick
+which elements you want to assign to a variable.
+
+*/
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [a,b] = [b,a];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
