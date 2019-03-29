591277f5edf852f54a1d0b7e1889cc30d4189b6a
mjtedder <mjtedder@gmail.com>
2019-03-28 21:21:19 -0600

    Import default export

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,8 @@
+/*
+To import a default export, we remove the curly braces from the import statement.  If we are importing multiple
+named exports, we surround them in curly braces after the import keyword.
+*/
+
+"use strict";
+import subtract from "math_functions"
+subtract(7, 4);
